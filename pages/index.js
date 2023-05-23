d63d8df16354927270b3d6567de6b694ed7888d6
Ericsson Augusto <68518266+ericssonaugusto@users.noreply.github.com>
2023-05-23 10:08:36 +0000

    Adciciona demais arquivos do projeto

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function Home(){
+    return <h1>Existe uma pessoa incrível que me completa, ela com seu jeito especial, mesmo tentando parecer durona, me acalma a alma, simplesmente com um olhar. Minha esposa CRLuz, Eu te Amo!</h1>
+}
