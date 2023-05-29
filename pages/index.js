e26d002145826e981ad4efc500e490749b9b3617
Ericsson Augusto <ericssonaugusto@gmail.com>
2023-05-29 19:19:40 -0300

    Inclusão via local

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home(){
-    return <h1>Existe uma pessoa incrível que me completa, ela com seu jeito especial, mesmo tentando parecer durona, me acalma a alma, simplesmente com um olhar. Minha esposa CRLuz, Eu te Amo!</h1>
+    return <h1>Existe uma pessoa incrível que me completa, ela com seu jeito especial, mesmo tentando parecer durona, me acalma a alma, simplesmente com um olhar. Minha esposa CRLuz, Eu te Amo!!!</h1>
 }
